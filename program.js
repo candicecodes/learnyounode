0771435626116206ff419de7023a1d184802b4e9
Candice Mayes <ca.mayes239@gmail.com>
2018-01-21 11:55:56 -0500

    completeted first i/o exercise

--- program.js
+++ program.js
@@ -1,13 +1,26 @@
-function add() {
+/* BABY STEPS ============================== */
+
+// function add() {
     // console.log(process.argv);
     //create a total variable and set it equal to 0
     //loop through process.argv array starting at process.argv[2] and ending at process.argv.length -1
     //at each index, add the value to total
     //return total
-    let argv = process.argv;
-    let total = 0;
-    for(let i = 2; i >= argv.length - 1; i++) {
-        total = total + Number(argv[i]);
-    }
-    console.log(total);
+//     let argv = process.argv;
+//     let total = 0;
+//     for(let i = 2; i <= argv.length - 1; i++) {
+//         total = total + Number(argv[i]);
+//     }
+//     console.log(total);
+// }
+// add(1, 2, 3);
+
+/* MY FIRST I/O! =========================== */
+
+function countLines(file) {
+    const fs = require('fs');
+    let bufferObject = fs.readFileSync(process.argv[2]);
+    let lines = bufferObject.toString().split('\n').length - 1;
+    console.log(lines);
 }
+countLines();
